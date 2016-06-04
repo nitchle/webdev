d0493333ae625dd9ad93014068b804fb2a0eb5c9
nikolaus.spindler@creamfinance.com <nikolaus.spindler@creamfinance.com>
2016-06-04 11:54:59 +0200

    added swig templates for index portfolio and about

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var app = require('express')(),
   	swig = require('swig');
-
 var express = require('express');
 
 // This is where all the magic happens!
@@ -24,48 +23,21 @@ swig.setDefaults({ cache: false });
 app.get('/', function (req, res) {
   res.render('index', { /* WTF ?! */});
 });
-
-app.get('/testswig', function (req, res) {
+app.get('/test', function (req, res) {
   res.render('testswig', { /* WTF ?! */});
 });
-
 app.get('/index', function (req, res) {
   res.render('index', { /* WTF ?! */});
 });
-
 app.get('/portfolio', function (req, res) {
   res.render('portfolio', { /* WTF ?! */});
 });
-
 app.get('/about', function (req, res) {
   res.render('about', { /* WTF ?! */});
 });
-
 app.get('*', function (req, res) {
   res.status(404).send('Sorry, the page you are requesting does not exist.');
 });
 
 app.listen(6666);
 console.log('Application Started on http://localhost:6666/');
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////
-//											FIRST TRY: 									 //
-// var express = require('express');
-// var path = require('path');
-// var app = express();
-// var swig = require('swig');
-
-// // Define the port
-// app.set('port', 3000);
-
-// app.use(express.static('webdev'));
-
-// // Listen for requests
-// var server = app.listen(app.get('port'), function() {
-//   var port = server.address().port;
-//   console.log('Waiting for connections on port ' + port);
-// });
-//																						 //
-///////////////////////////////////////////////////////////////////////////////////////////
